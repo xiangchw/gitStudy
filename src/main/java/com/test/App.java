5c98376c19ea175cfd531334695d8ceb20d48a9a
xiangchw <xiangchw789@gmail.com>
2021-08-06 11:09:40 +0800

    第二次提交

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -1,13 +1,18 @@
 package com.test;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(randomString());
+    }
+
+    private static String randomString() {
+        return String.valueOf(ThreadLocalRandom.current().nextDouble());
     }
 }
+
