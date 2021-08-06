4e33b5e3e3a89cc42d5d72466073ef0a8b231cc6
xiangchw <xiangchw789@gmail.com>
2021-08-06 14:12:58 +0800

    问题追踪

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -9,6 +9,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("HEllo World!");
         System.out.println(randomString());
+        System.out.println("测试绑定一个问题追踪地址:");
     }
 
     private static String randomString() {
