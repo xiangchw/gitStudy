e1ff66e081eb6146f72cbd3979aaea1e8a29418a
xiangchw <xiangchw789@gmail.com>
2021-08-10 09:54:12 +0800

    master测试冲突
    Signed-off-by: xiangchw <xiangchw789@gmail.com>

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -14,12 +14,13 @@ public class App {
         System.out.println(randomString());
         System.out.println("测试绑定一个问题追踪地址:");
         System.out.println("Master增加新功能!");
+        System.out.println("Master测试冲突!");
         System.out.println("测试绑定一个问题追踪地址: 9041");
         System.out.println("rebase新增功能2");
         System.out.println("master新增功能2");
         System.out.println("rebase新增功能3");
         System.out.println("rebase新增功能4");
-        System.out.println("REBASE新增功能5");
+        System.out.println("REBASE测试冲突");
         System.out.println("MASTER新增功能5");
     }
 
